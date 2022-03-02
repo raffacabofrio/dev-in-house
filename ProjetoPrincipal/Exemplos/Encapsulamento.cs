96ed9717df17a2078c9f1e58d2b5d5cdf3e4fd3e
Raffaello Damgaard Neves <raffaello.neves@pixeon.com>
2022-03-01 21:19:37 -0300

    Exemplo encapsulamento.

---
+++ ProjetoPrincipal/Exemplos/Encapsulamento.cs
@@ -0,0 +1,61 @@
+﻿using static System.Console;
+
+
+public class Encapsulamento
+{
+    public void Exemplos()
+    {
+        var r = new Retangulo();
+        WriteLine("Informe o comprimento: ");
+        r.Comprimento = Convert.ToDouble(Console.ReadLine());
+        WriteLine("Informe a largura : ");
+        r.Largura = Convert.ToDouble(Console.ReadLine());
+        WriteLine($"Area do Retangulo: {r.GetArea()}");
+    }
+
+}
+
+class Retangulo
+{
+    //variáveis membros
+    private double comprimento;
+    private double largura;
+
+    public double Comprimento
+    {
+        get { return comprimento; }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("O valor do comprimento não pode ser menor que zero");
+            }
+            else
+            {
+                comprimento = value;
+            }
+        }
+    }
+
+    public double Largura
+    {
+        get { return largura; }
+        set
+        {
+            if (value < 0)
+            {
+                throw new Exception("O valor da largura não pode ser menor que zero");
+            }
+            else
+            {
+                largura = value;
+            }
+        }
+    }
+
+    public double GetArea()
+    {
+        return Comprimento * Largura;
+    }
+}
+
--- ProjetoPrincipal/Program.cs
+++ ProjetoPrincipal/Program.cs
@@ -1,12 +1,24 @@
 ﻿
+
 //var linq = new LinqExemplo();
 //linq.Exemplos();
 
 //var exemploCtor = new ConstructorDestructor();
 //exemploCtor.Exemplos();
 
-var modificadores = new Modificadores();
-modificadores.Exemplos();
+//var modificadores = new Modificadores();
+//modificadores.Exemplos();
+
+//var encapsulamento = new Encapsulamento();
+//encapsulamento.Exemplos();
+
+
+
+
+
+
+
+
 
 
 
